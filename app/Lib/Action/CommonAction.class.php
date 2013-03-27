b2cf973b2b21a3dc508bfb7e68e60d25fa7428dc
minch <yeah@minch.me>
2013-03-27 14:55:48 +0800

    add captcha methods in common action class

--- app/Lib/Action/CommonAction.class.php
+++ app/Lib/Action/CommonAction.class.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * 系统公共Action类
+ * @author minch <yeah@minch.me>
+ * @link http://www.minch.me
+ */
 class CommonAction extends Action
 {
 	/**
@@ -22,5 +26,31 @@ class CommonAction extends Action
 	    		C(strtoupper($v['skey']),$v['svalue']);
 	    	}
 	    }
+	}
+	
+	/**
+	 * 验证码
+	 */
+	public function captcha()
+	{
+		import('ORG.Util.Image');
+		$length = intval($_GET['length']) ? intval($_GET['length']) : 4;
+		$mode = 2;
+		$type = 'png';
+		$width = intval($_GET['width']) ? intval($_GET['width']) : 50;
+		$height = intval($_GET['height']) ? intval($_GET['height']) : 25;
+		$sessame = 'captcha';
+		Image::buildImageVerify($length, $mode, $type, $width, $height, $sessame);
+	}
+	
+	/**
+	 * 检查验证码是否正确
+	 * @param string $captcha 验证码
+	 * @return boolean
+	 */
+	protected function checkCaptcha($captcha = '')
+	{
+		$captcha = $captcha ? $captcha : strval($_REQUEST['captcha']);
+		return md5(strtoupper($captcha)) == session('captcha');
 	}
 }
