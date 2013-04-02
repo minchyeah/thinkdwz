13de1591faff2e2d1b38ee79e0d5c9eb7fbe5de9
minch <yeah@minch.me>
2013-04-02 23:25:43 +0800

    change Home Module public templates
    Change-Id: I251e9a5ab34ea1190d0b3339bf3af67801142d86

--- app/Conf/config.php
+++ app/Conf/config.php
@@ -6,6 +6,12 @@ $config = array(
     'APP_GROUP_PATH' 		=> 'Modules', // 分组目录 独立分组模式下面有效
     'TMPL_ACTION_ERROR'     => 'Public:error', // 默认错误跳转对应的模板文件
     'TMPL_ACTION_SUCCESS'   => 'Public:success', // 默认成功跳转对应的模板文件
+	'TMPL_PARSE_STRING' => array( // 模板内容替换
+			' src="image' => ' src="'.__ROOT__.'/static/image',
+			' href="css/' => ' href="'.__ROOT__.'/static/css/',
+			' src="js/' => ' src="'.__ROOT__.'/static/js/',
+			' src="upload/' => ' src="'.__ROOT__.'/data/upload/',
+		),
 	'URL_CASE_INSENSITIVE'  => true,   // 默认false 表示URL区分大小写 true则表示不区分大小写
 	'URL_MODEL'             => 2,       // URL访问模式,0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式，提供最好的用户体验和SEO支持
 	'URL_HTML_SUFFIX'       => '.html',  // URL伪静态后缀设置
