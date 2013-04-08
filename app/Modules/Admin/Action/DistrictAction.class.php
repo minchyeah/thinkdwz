01c85ba492ede69b6331551428d9e830ee59bed4
minch <yeah@minch.me>
2013-04-09 01:17:19 +0800

    初始化其它功能模块
    Change-Id: I9026127a3ecc17b4bd9b0bc3c37b10a3903bdeb1

---
+++ app/Modules/Admin/Action/DistrictAction.class.php
@@ -0,0 +1,59 @@
+<?php
+
+class DistrictAction extends AdminAction
+{
+    public function index()
+    {
+    	$model = D('Users');
+    	$totalCount = $model->count();
+    	$currentPage = intval($_REQUEST['pageNum']);
+    	$currentPage = $currentPage ? $currentPage : 1;
+    	$numPerPage = 20;
+    	$rowOffset = ($currentPage-1) * $numPerPage;
+    	$list = $model->order('id DESC')->limit($rowOffset . ',' . $numPerPage)->select();
+    	
+    	$this->assign('list', $list);
+    	$this->assign('totalCount', $totalCount);
+    	$this->assign('numPerPage', $numPerPage);
+    	$this->assign('currentPage', $currentPage);
+        $this->display();
+    }
+    
+    public function add()
+    {
+    	$model = D('Users');
+    	$this->assign('topmenus', $model->topMenus());
+    	$this->display();
+    }
+    
+    public function edit()
+    {
+    	$id =  intval($_GET['id']);
+    	$model = D('Users');
+    	$this->assign('topmenus', $model->topMenus());
+    	$this->assign('vo', $model->find($id));
+    	$this->display('add');
+    }
+    
+    public function save()
+    {
+    	$model = D('Users');
+    	$data = $model->create();
+    	$data['params'] = '';
+    	$data['group_name'] = '';
+    	if(!$data){
+    		$this->error($model->getError());
+    	}
+    	if (!$data['id']) {
+    		$rs = $model->add();
+    	}else{
+    		$rs = $model->save();
+    	}
+    	if(false !== $rs){
+    		$this->success('保存成功！');
+    	}else{
+    		$this->error('保存失败！'.dump($data, false).$model->getDbError());
+    	}
+    }
+}
+?>
