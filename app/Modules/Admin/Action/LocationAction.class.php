12403eafd8f8b6787dfff8a7e0dd1fbff1681965
minch <yeah@minch.me>
2013-04-10 00:21:43 +0800

    商圈功能
    Change-Id: I5f302f87b45c6aae778371e657365fa2cf69f65a

--- app/Modules/Admin/Action/LocationAction.class.php
+++ app/Modules/Admin/Action/LocationAction.class.php
@@ -4,7 +4,7 @@ class LocationAction extends AdminAction
 {
     public function index()
     {
-    	$model = D('Users');
+    	$model = D('Locations');
     	$totalCount = $model->count();
     	$currentPage = intval($_REQUEST['pageNum']);
     	$currentPage = $currentPage ? $currentPage : 1;
@@ -12,35 +12,55 @@ class LocationAction extends AdminAction
     	$rowOffset = ($currentPage-1) * $numPerPage;
     	$list = $model->order('id DESC')->limit($rowOffset . ',' . $numPerPage)->select();
     	
+    	$model = D('District');
+    	$rs = $model->select();
+    	$district = array();
+    	if (is_array($rs)) {
+    		foreach ($rs as $v){
+    			$district[$v['id']] = $v;
+    		}
+    	}
+    	
     	$this->assign('list', $list);
     	$this->assign('totalCount', $totalCount);
     	$this->assign('numPerPage', $numPerPage);
     	$this->assign('currentPage', $currentPage);
+    	$this->assign('district', $district);
         $this->display();
     }
     
     public function add()
     {
-    	$model = D('Users');
-    	$this->assign('topmenus', $model->topMenus());
+    	$this->_assign_district();
     	$this->display();
+    }
+    
+    private function _assign_district()
+    {
+    	$model = D('District');
+    	$data = $model->order('sort_order ASC')->select();
+    	$this->assign('district', $data);
     }
     
     public function edit()
     {
     	$id =  intval($_GET['id']);
-    	$model = D('Users');
-    	$this->assign('topmenus', $model->topMenus());
+    	$model = D('Locations');
     	$this->assign('vo', $model->find($id));
+    	$this->_assign_district();
     	$this->display('add');
     }
     
     public function save()
     {
-    	$model = D('Users');
+    	import('@.Util.Pinyin');
+    	$pinyin = new Pinyin();
+    	$alias = str_replace(' ', '', $pinyin->output($_REQUEST['title']));
+    	$letter = strtoupper(substr($alias, 0, 1));
+    	$_POST['alias'] = $alias;
+    	$_POST['letter'] = $letter;
+    	$model = D('Locations');
     	$data = $model->create();
-    	$data['params'] = '';
-    	$data['group_name'] = '';
     	if(!$data){
     		$this->error($model->getError());
     	}
