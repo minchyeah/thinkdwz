1fa85fad306e5082bee951e59356c4458874d116
minch <yeah@minch.me>
2013-06-20 23:47:07 +0800

    数据库表更新

--- data/backup/database.sql
+++ data/backup/database.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50524
 File Encoding         : 65001
 
-Date: 2013-06-10 16:04:05
+Date: 2013-06-20 23:44:47
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -39,9 +39,7 @@ CREATE TABLE `admin` (
 -- ----------------------------
 -- Records of admin
 -- ----------------------------
-INSERT INTO `admin` VALUES ('1', 'admin', '0db29d9f6881308351aa13f9261203c8', 'XanOhFRd', '管理员', 'admin@admin.com', '', '1', '127.0.0.1', '11', '1370846905', '1367845114');
-INSERT INTO `admin` VALUES ('2', 'nanning', '7b149d9d177ca300a076c4d30e484801', 'gMNejrdA', '南宁', 'nanning@test.cn', '3', '1', '127.0.0.1', '1', '1367847953', '1367847127');
-INSERT INTO `admin` VALUES ('3', 'test', 'f629d2ea167fd1d2d5094149b9dfa712', 'bcGiCkjr', 'admin', 'nanning@test.cn', '5', '1', '', '0', '0', '1367847224');
+INSERT INTO `admin` VALUES ('1', 'admin', '0db29d9f6881308351aa13f9261203c8', 'XanOhFRd', '管理员', 'admin@admin.com', '', '1', '127.0.0.1', '14', '1371740939', '1367845114');
 
 -- ----------------------------
 -- Table structure for `admin_menus`
@@ -60,7 +58,7 @@ CREATE TABLE `admin_menus` (
   `remark` varchar(500) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   KEY `pid` (`pid`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of admin_menus
@@ -75,7 +73,7 @@ INSERT INTO `admin_menus` VALUES ('7', '产品列表', '', 'Product', 'index', '
 INSERT INTO `admin_menus` VALUES ('8', '区域划分', '', 'District', 'index', '', '2', '3', '0', '');
 INSERT INTO `admin_menus` VALUES ('9', '文章列表', '', 'Article', 'index', '', '4', '1', '1', '');
 INSERT INTO `admin_menus` VALUES ('10', '友情链接', '', 'Link', 'index', '', '4', '5', '0', '');
-INSERT INTO `admin_menus` VALUES ('11', '商圈划分', '', 'Location', 'index', '', '2', '4', '0', '');
+INSERT INTO `admin_menus` VALUES ('11', '幻灯片', '', 'Slider', 'index', '', '4', '7', '1', '');
 INSERT INTO `admin_menus` VALUES ('12', '广告管理', '', 'Advertise', 'index', '', '4', '5', '0', '');
 INSERT INTO `admin_menus` VALUES ('13', '关于我们', '', 'ArticlePage', 'aboutus', '', '4', '3', '0', '');
 INSERT INTO `admin_menus` VALUES ('14', '文章栏目', '', 'Article', 'category', '', '4', '2', '1', '');
@@ -85,6 +83,7 @@ INSERT INTO `admin_menus` VALUES ('17', '管理员用户', '', 'User', 'admin', 
 INSERT INTO `admin_menus` VALUES ('18', '联络我们', '', 'ArticlePage', 'contact', '', '4', '3', '1', '');
 INSERT INTO `admin_menus` VALUES ('19', '免责声明', '', 'ArticlePage', 'terms', '', '4', '3', '0', '');
 INSERT INTO `admin_menus` VALUES ('20', '品牌管理', '', 'Product', 'brand', '', '2', '2', '0', '');
+INSERT INTO `admin_menus` VALUES ('21', '招贤纳士', '', 'Jobs', 'index', '', '4', '8', '1', '');
 
 -- ----------------------------
 -- Table structure for `advertise`
@@ -128,14 +127,11 @@ CREATE TABLE `articles` (
   KEY `cate_id` (`cate_id`),
   KEY `user_id` (`user_id`),
   KEY `status` (`status`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=147 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of articles
 -- ----------------------------
-INSERT INTO `articles` VALUES ('1', '新闻标题士大夫', '<img src=\"/cart/data/upload/n/fl/51ab50495fcf8.jpg\" alt=\"\" />棋花样百出塔顶地花样百出花样百出花样百出花样百出花样百出士大夫 士大夫 <br />', '', '', '0', '0', '0', '', '', '', '3', '枯枯枯', '0');
-INSERT INTO `articles` VALUES ('2', '花样百出花样百出花样百出', '<img src=\"/cart/data/upload/j/vk/51ab5165d4be2_600x480.jpg\" alt=\"\" />花样百出塔顶模压 模压 花样百出棋花样百出塔顶<br />', '', '', '0', '8', '0', '', '', '', '3', 'dfeddd', '1');
-INSERT INTO `articles` VALUES ('3', '新闻二的标题', '<p>花样百出花样百出棋</p><p><img src=\"/cart/data/upload/z/vq/51ab61a651d2c_600x480.jpg\" alt=\"\" /><br /></p>', '', '', '1370186220', '9', '1', '', '', '', '4', '网络', '1');
 
 -- ----------------------------
 -- Table structure for `article_category`
@@ -181,14 +177,33 @@ CREATE TABLE `article_page` (
   `visit_count` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `page_code` (`page_code`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of article_page
 -- ----------------------------
-INSERT INTO `article_page` VALUES ('1', 'contact', '联系我们', '联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联<img src=\"/cart/data/upload/v/of/51ab540655a71_600x480.jpg\" alt=\"\" />系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们', '', '', '', '1370182675', '0');
-INSERT INTO `article_page` VALUES ('2', '', '阳xf dfdsfv  asdv ', 'xac av c vsdvxcv xc v<br />', '', '', '', '1370098284', '0');
-INSERT INTO `article_page` VALUES ('3', '', '花样百出', null, '', '', '士大夫仍未遂犯', '1370188213', '0');
+INSERT INTO `article_page` VALUES ('1', 'contact', '联系我们', '<p>联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联</p><p align=\"center\"><img src=\"/cart/data/upload/70/91/51b6ff1178437_600x480.jpg\" alt=\"\" /><br /></p><p>系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们</p>', '', '', '我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们联系我们', '1370947374', '23');
+
+-- ----------------------------
+-- Table structure for `jobs`
+-- ----------------------------
+DROP TABLE IF EXISTS `jobs`;
+CREATE TABLE `jobs` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `requirement` text NOT NULL,
+  `description` text NOT NULL,
+  `number` smallint(5) unsigned NOT NULL DEFAULT '1',
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of jobs
+-- ----------------------------
+INSERT INTO `jobs` VALUES ('1', '业务员', '专业要求：服务专业\n学历要求：中专以上\n年龄要求：18—35岁\n性别要求：男性\n工作性质：全职\n工作区域：全国\n工作经验：有一年以上工作经验者优先\n薪酬待遇：年薪5-10万（基本工资+绩效考核+年功工资+提成工资+六险+补贴+激励奖+年终奖）', '岗位职责：\n1、建立、管理、维护销售部文件数据档案，为部门制定正确的销售方案、销售计划提供决策依据；\n2、对产品销售过程的管理；\n3、草拟年季月度销售和预算计划；\n4、负责账务的核对；\n5、监督、协调各网点的营运情况。', '10', '1');
+INSERT INTO `jobs` VALUES ('2', '技术员', '    专业要求：服务专业\r\n    学历要求：中专以上\r\n    年龄要求：18—35岁\r\n    性别要求：女性\r\n    工作性质：广西.南宁\r\n    工作经验：有一年以上工作经验者优先\r\n    薪酬待遇：年薪5-10万（基本工资+绩效考核+年功工资+提成工资+六险+补贴+激励奖+年终奖）', '岗位职责：\r\n1、建立、管理、维护销售部文件数据档案，为部门制定正确的销售方案、销售计划提供决策依据；\r\n2、对产品销售过程的管理；', '2', '1');
+INSERT INTO `jobs` VALUES ('3', '公司前台', '    专业要求：服务专业\r\n    学历要求：中专以上\r\n    性别要求：男性\r\n    工作区域：全国\r\n    工作经验：有一年以上工作经验者优先', '岗位职责：\r\n1、建立、管理、维护销售部文件数据档案，为部门制定正确的销售方案、销售计划提供决策依据；\r\n2、对产品销售过程的管理；\r\n3、草拟年季月度销售和预算计划；\r\n4、负责账务的核对；', '1', '1');
 
 -- ----------------------------
 -- Table structure for `links`
@@ -227,14 +242,15 @@ CREATE TABLE `products` (
   PRIMARY KEY (`id`),
   KEY `cate_id` (`cate_id`),
   CONSTRAINT `FK_Reference_2` FOREIGN KEY (`cate_id`) REFERENCES `product_category` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of products
 -- ----------------------------
-INSERT INTO `products` VALUES ('1', '顶戴枯花样百出棋', '', '花样百出塔顶地', '', '2', null, '花样百出棋模压 <br />', '0', '1', null);
+INSERT INTO `products` VALUES ('1', '顶戴枯花样百出棋', 'upload/38/80/51b73e5892925.jpg', '花样百出塔顶地', '', '2', '大师傅花样百出花样百出', '花样百出棋模压 <br />', '0', '1', 'upload/38/80/51b73e5892925.jpg,upload/01/03/51b73e590d4f0.jpg,upload/40/42/51b7408287855.jpg');
 INSERT INTO `products` VALUES ('2', '顶戴枯花样百出棋人枯', '', '花样百出花样百出', '', '2', '花样百出花样百出花样百出枯地', '塔顶时时有仍地轩叶直睦基址 草木灰睦共产党亲友楔<br />', '1370188704', '0', null);
-INSERT INTO `products` VALUES ('3', '顶戴枯花样百出棋', 'upload/20/09/51b57abc300f4.jpg', 'dsfdasf asdf', '', '5', ' asdf asdfsad fasdf', 'sad sad sd f<br />', '1370848671', '1', 'upload/20/09/51b57abc300f4.jpg,upload/69/06/51b57abc9498d.jpg');
+INSERT INTO `products` VALUES ('3', '顶戴枯花样百出棋', 'upload/20/09/51b57abc300f4.jpg', 'dsfdasf asdf', '', '5', '士大夫：枯夺 士大夫枯\r\n士大： 地士大夫\r\n饿地：厅枯 厅在', 'sad sad sd f<br />', '1370848671', '1', 'upload/20/09/51b57abc300f4.jpg,upload/69/06/51b57abc9498d.jpg');
+INSERT INTO `products` VALUES ('4', '灰机机啊', 'upload/27/16/51b73ad154d2f.jpg', '缀枯花样百出柑棋档案室霜期地大师傅', '', '4', '花样百出塔顶枯棋要夺', '<p>枯花样百出士大夫士大夫</p><p align=\"center\"><img src=\"/cart/data/upload/22/45/51b73ae5ead4b_600x480.jpg\" alt=\"\" /></p>', '1370962677', '1', 'upload/27/16/51b73ad154d2f.jpg,upload/07/91/51b73ad1b8bde.jpg,upload/19/34/51b73ad21d291.jpg');
 
 -- ----------------------------
 -- Table structure for `product_category`
@@ -288,6 +304,8 @@ INSERT INTO `settings` VALUES ('contact_phone', '020-279123787', '', '', '', '',
 INSERT INTO `settings` VALUES ('copyright', 'Copyright ©2013 All Rights Reserved.', '', '', '', '', '');
 INSERT INTO `settings` VALUES ('hot_keywords', '', '', '', '', '', '');
 INSERT INTO `settings` VALUES ('icp_info', '', '', '', '', '', '');
+INSERT INTO `settings` VALUES ('jobs_contact', '联系人：陈小姐\r\n联系电话：0771-3219952\r\n详细地址：广州大道41号国贸中心A座28楼2812\r\n投递邮箱：rzp@163.com g', '', '', '', '', '');
+INSERT INTO `settings` VALUES ('jobs_description', '在新兴经济体，卡特彼勒机械正被用来建设道路，机场，医院和排污系统，创造经济机会和提高生活水平。在发达经济体，卡特彼勒网络的经销商与顾客一起共同创新解决方案。gggggg', '', '', '', '', '');
 INSERT INTO `settings` VALUES ('kefu_phone', '020-679123787', '', '', '', '', '');
 INSERT INTO `settings` VALUES ('min_withdraw', '0', '', '', '', '', '');
 INSERT INTO `settings` VALUES ('seo_description', '', '', '', '', '', '');
@@ -295,3 +313,23 @@ INSERT INTO `settings` VALUES ('seo_keywords', '', '', '', '', '', '');
 INSERT INTO `settings` VALUES ('sitename', '广州城铁机械', '', '', '', '', '');
 INSERT INTO `settings` VALUES ('stat_code', '', '', '', '', '', '');
 INSERT INTO `settings` VALUES ('web_admin_pagenum', '0', '', '', '', '', '');
+
+-- ----------------------------
+-- Table structure for `slider`
+-- ----------------------------
+DROP TABLE IF EXISTS `slider`;
+CREATE TABLE `slider` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `target` varchar(255) NOT NULL DEFAULT '',
+  `image` varchar(255) NOT NULL DEFAULT '',
+  `position` varchar(20) NOT NULL DEFAULT '',
+  `sort_order` smallint(5) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `position` (`position`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of slider
+-- ----------------------------
+INSERT INTO `slider` VALUES ('1', '村大梵蒂冈', 'http://test.com/sdfdfdfd', 'upload/04/46/51c31e1a71b68.jpg', 'index', '1');
