47dbaa3716db76e3f4e8979f833f716f1bb71f91
Minch <yeah@minch.me>
2015-08-11 22:36:13 +0800

    完善案例功能

--- app/Modules/Admin/Action/CaseAction.class.php
+++ app/Modules/Admin/Action/CaseAction.class.php
@@ -4,10 +4,8 @@ class CaseAction extends AdminAction
 {
 	public function index()
 	{
-		$model = D('Stores');
-		$location_id = intval($_REQUEST['location_id']);
+		$model = D('Cases');
 		$where = '';
-		$location_id && $where = 'FIND_IN_SET('.$location_id.',`locations`)';
 		$key = trim(strval($_REQUEST['search_key']));
 		if($key){
 			$where .= ' AND ( `name` LIKE "%'.$key.'%" OR `address` LIKE "%'.$key.'%" )';
@@ -24,36 +22,21 @@ class CaseAction extends AdminAction
 		$this->assign('totalCount', $totalCount);
 		$this->assign('numPerPage', $numPerPage);
 		$this->assign('currentPage', $currentPage);
-		$this->assign('location_id', $location_id);
-		$this->assign('locations', F('locations'));
 		$this->display();
 	}
 	
 	public function add()
 	{
-		$location_id = intval($_REQUEST['location_id']);
-		$location = D('Locations')->find($location_id);
-		$vo['city_id'] = $location['city_id'];;
-		$vo['locations'] = $location_id;
-		$vo['locationsName'] = $location['title'];
-		$this->assign('vo', $vo);
-		$this->assign('location_id', $location_id);
 		$this->display();
 	}
 
 	public function delete()
 	{
-		$model = D('Stores');
+		$model = D('Cases');
 		$model->startTrans();
 		$id = intval($_REQUEST['id']);
 		$where = array();
 		$where['id'] = $id;
-		$orgStore = $model->find($id);
-		$dids = explode(',', $orgStore['locations']);
-		foreach ($dids as $v){
-			$lid = intval($v);
-			$lid && $model->where(array('id'=>$lid))->setDec('store_count');
-		}
 		$rs = $model->where($where)->delete();
 		if($rs){
 			$model->commit();
@@ -67,52 +50,32 @@ class CaseAction extends AdminAction
 	public function edit()
 	{
 		$id =  intval($_GET['id']);
-		$model = D('Stores');
-		$store = $model->find($id);
-		$store['locationsName'] = $this->ids2str($store['locations'], D('Locations'), 'title');
-		$store['type_name'] = $this->ids2str($store['types'], D('StoreType'), 'type_name');
-		$this->assign('vo', $store);
+		$model = D('Cases');
+		$case = $model->find($id);
+		$this->assign('vo', $case);
 		$this->display('add');
 	}
 	
 	public function save()
 	{
-		$model = D('Stores');
+		$model = D('Cases');
 		if ($_FILES['imgfile']['name']) {
 			$image = $this->saveImage($_FILES['imgfile']);
 			if ($image) {
 				$_POST['image'] = $image;
 			}
 		}
-		$_POST['recommend'] = intval($_POST['recommend']);
 		$_POST['sort_order'] = intval($_POST['sort_order']);
 		$data = $model->create();
 		if(!$data){
 			$this->error($model->getError());
 		}
-		$lModel = D('Locations');
 		$model->startTrans();
 		if (!$data['id']) {
 			$data['dateline'] = time();
 			$rs = $model->add($data);
-			$dids = explode(',', $data['locations']);
-			foreach ($dids as $v){
-				$lid = intval($v);
-				$lid && $lModel->where(array('id'=>$lid))->setInc('store_count');
-			}
 		}else{
-			$orgStore = $model->find($data['id']);
-			$dids = explode(',', $orgStore['locations']);
-			foreach ($dids as $v){
-				$lid = intval($v);
-				$lid && $lModel->where(array('id'=>$lid))->setDec('store_count');
-			}
 			$rs = $model->save($data);
-			$dids = explode(',', $data['locations']);
-			foreach ($dids as $v){
-				$lid = intval($v);
-				$lid && $lModel->where(array('id'=>$lid))->setInc('store_count');
-			}
 		}
 		if(false !== $rs){
 			$model->commit();
--- app/Modules/Home/Action/CasesAction.class.php
+++ app/Modules/Home/Action/CasesAction.class.php
@@ -4,6 +4,24 @@ class CasesAction extends HomeAction
 {
 	public function index()
 	{
+	    $model = M('Cases');
+	    $where = array('state'=>1);
+	    $total = $model->field('COUNT(1) count')->where($where)->select();
+	    $total_count = intval($total[0]['count']);
+	    $current_page = intval($_GET['page']);
+	    $current_page = $current_page ? $current_page : 1;
+	    $total_page = ceil($total_count/6);
+	    $pre_page = '';
+	    $next_page = '';
+	    
+	    $volist = $model->where($where)->limit(($current_page-1)*6, 6)->select();
+	    
+	    $this->assign('current_page', $current_page);
+	    $this->assign('total_page', $total_page);
+	    $this->assign('total_count', $total_count);
+	    $this->assign('pre_page', $pre_page);
+	    $this->assign('next_page', $next_page);
+	    $this->assign('volist', $volist);
 		$this->display();
 	}
 }
