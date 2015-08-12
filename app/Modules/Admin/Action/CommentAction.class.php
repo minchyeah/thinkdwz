012872d80f0a7a0926f923fe015fd77cf4c7762a
Minch <yeah@minch.me>
2015-08-13 00:18:59 +0800

    sdf

--- app/Modules/Admin/Action/CommentAction.class.php
+++ app/Modules/Admin/Action/CommentAction.class.php
@@ -4,6 +4,24 @@ class CommentAction extends AdminAction
 {
 	public function index()
 	{
+		$model = D('Msgboard');
+		$where = '';
+		$key = trim(strval($_REQUEST['search_key']));
+		if($key){
+			$where .= ' AND `product_name` LIKE "%'.$key.'%"';
+		}
+		$where = trim($where, ' AND');
+		$totalCount = $model->where($where)->count();
+		$currentPage = intval($_REQUEST['pageNum']);
+		$currentPage = $currentPage ? $currentPage : 1;
+		$numPerPage = 20;
+		$rowOffset = ($currentPage-1) * $numPerPage;
+		$list = $model->where($where)->order('id DESC')->limit($rowOffset . ',' . $numPerPage)->select();
+		
+		$this->assign('list', $list);
+		$this->assign('totalCount', $totalCount);
+		$this->assign('numPerPage', $numPerPage);
+		$this->assign('currentPage', $currentPage);
 		$this->display();
 	}
 	
