60846e2bc644b65d176e47cd0c0753fc44b6e3fe
Minch <yeah@minch.me>
2015-08-13 22:22:18 +0800

    sdff sdf asd

--- app/Modules/Home/Model/MsgboardModel.class.php
+++ app/Modules/Home/Model/MsgboardModel.class.php
@@ -4,9 +4,9 @@ class MsgboardModel extends Model
 {
 	public $_validate = array(
 			array('name','require','请输入您的名字'),
+			array('mobile','require','请输入您的电话'),
 			array('mobile','/^[0-9\-]{5,16}$/i','电话格式错误', 0),
 			array('email','email','Email格式错误', 0),
-			array('address','require','请输入您的地址'),
 			array('content','require','请输入留言内容'),
 		);
 }
