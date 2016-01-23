5f5a0cde90a466839c4cc68220716acd12509dde
Minch <yeah@minch.me>
2016-01-23 21:26:26 +0800

    perfect

--- app/Modules/Home/Action/CourseAction.class.php
+++ app/Modules/Home/Action/CourseAction.class.php
@@ -41,5 +41,78 @@ class CourseAction extends HomeAction
 	        return $this->detail($name);
 	    }
 	}
+
+	public function order()
+	{
+	    $model = D('Order');
+	    if($_POST['contact'] == '请输入您的姓名...'){
+	        $_POST['contact'] = '';
+	    }
+	    if($_POST['phone'] == '请输入您的联系电话...'){
+	        $_POST['phone'] = '';
+	    }
+	    if($_POST['address'] == '请输入您的联系地址...'){
+	        $_POST['address'] = '';
+	    }
+	    if($_POST['captcha'] == ''){
+	        $this->error('请输入验证码');
+	    }
+	    if(!$this->checkCaptcha()){
+	        $this->error('验证码错误');
+	    }
+	    $_POST['ip'] = get_client_ip();
+	    $data = $model->create();
+	    if(!$data){
+	        $this->error($model->getError());
+	    }
+	    $model->startTrans();
+	    if (!$data['id']) {
+	        $data['dateline'] = time();
+	        $rs = $model->add($data);
+	    }else{
+	        $rs = $model->save($data);
+	    }
+	    if(false !== $rs){
+	        $model->commit();
+	        $data['dateline'] = date('Y-m-d H:i', $data['dateline']);
+	        $this->mailorder($data);
+	        $this->success('预订成功！');
+	    }else{
+	        $model->rollback();
+	        $this->error('预订失败！');
+	    }
+	}
+
+	private function mailorder($data)
+	{
+	    $tomail = C('notify_email');
+	    $subject = '您有新的课程预订!';
+	    $body = <<<BODY
+	    <body>
+	        <h2>您有新的课程预订</h2>
+	        <table class="table" width="100%">
+                <thead>
+            	  <tr style="text-align:left;">
+            		<th>预订课程</th>
+            		<th>联系人</th>
+            		<th>联系电话</th>
+            		<th>联系地址</th>
+            		<th>预订时间</th>
+            	  </tr>
+                </thead>
+                <tbody>
+            	  <tr style="text-align:left;">
+            		<td>{$data['course_name']}</td>
+            		<td>{$data['contact']}</td>
+            		<td>{$data['phone']}</td>
+            		<td>{$data['address']}</td>
+            		<td>{$data['dateline']}</td>
+            	  </volist>
+                </tbody>
+              </table>
+	    </body>
+BODY;
+	    $this->sendmail($tomail, $subject, $body);
+	}
 }
 ?>
--- app/Modules/Home/Model/OrderModel.class.php
+++ app/Modules/Home/Model/OrderModel.class.php
@@ -2,12 +2,13 @@
 
 class OrderModel extends Model
 {
-    protected $trueTableName    =   'product_orders';
+    protected $trueTableName    =   'course_orders';
     
 	public $_validate = array(
-			array('contact','require','请输入您的名字'),
+			array('contact','require','请输入您的姓名'),
 			array('phone','require','请输入您的电话'),
-			array('phone','/^[0-9\-]{5,16}$/i','电话格式错误', 0),
+			array('phone','/^[0-9\-]{5,16}$/i','请正确输入您的电话', 0),
+			array('age','/^[0-9]{1,2}$/i','请正确输入年龄', 0),
 			array('address','require','请输入您地址'),
 		);
 }
