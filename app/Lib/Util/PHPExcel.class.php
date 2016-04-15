6dacbebdca0e0c7be959025df57f671ca673322b
梁敏成 <yeah@minch.me>
2016-04-15 10:06:17 +0800

    update PHPExcel

--- app/Lib/Util/PHPExcel.class.php
+++ app/Lib/Util/PHPExcel.class.php
@@ -1,175 +1,347 @@
 <?php
 
 /**
- * Simple excel generating from PHP5
- *
- * @package Utilities
- * @license http://www.opensource.org/licenses/mit-license.php
- * @author Oliver Schwarz <oliver.schwarz@gmail.com>
- * @version 1.0
+ * Excel Xml Generator
+ * 
+ * @category Utilities
+ * @package  Utilities
+ * @author   Oliver Schwarz <oliver.schwarz@gmail.com>
+ * @license  MIT License http://www.opensource.org/licenses/mit-license.php
+ * @version  2.0
+ * @link     http://code.google.com/p/php-excel/
+ * 
+ * @todo Add compatibility methods for version 1.0
+ * @todo Add method to write to file
+ * @todo Add method to return output
+ * @example 
+ * 
+ * require 'PHPExcel.class.php';
+ * $data = array();
+ * for ($i = 1; $i < 200; $i++) {
+ *     $data[] = array($i, 'My test');
+ * }
+ * $data_sec = array();
+ * for ($i = 1; $i < 110; $i++) {
+ *     $data_sec[] = array($i, '109 times my name');
+ * }
+ * $phpexcel = new PHPExcel;
+ * $phpexcel->addWorksheet('Worksheet test', $data);
+ * $phpexcel->addWorksheet('Worksheet test 2', $data_sec);
+ * $phpexcel->sendWorkbook('openoffice-test.xml');
+ * 
  */
 
 /**
- * Generating excel documents on-the-fly from PHP5
- * 
- * Uses the excel XML-specification to generate a native
- * XML document, readable/processable by excel.
- * 
+ * Excel Xml Generator Class
+ *
+ * A simple class to dump an array into a file or output stream
+ * readable by Excel or other calc/spreadsheet software.
+ *
+ * @category Utilities
  * @package Utilities
- * @subpackage Excel
- * @author Oliver Schwarz <oliver.schwarz@vaicon.de>
- * @version 1.1
- * 
- * @todo Issue #4: Internet Explorer 7 does not work well with the given header
- * @todo Add option to give out first line as header (bold text)
- * @todo Add option to give out last line as footer (bold text)
- * @todo Add option to write to file
+ * @author Oliver Schwarz <oliver.schwarz@gmail.com>
+ * @license MIT license http://www.opensource.org/licenses/mit-license.php
+ * @version 2.0
+ * @link http://code.google.com/p/php-excel/
  */
 class PHPExcel
 {
-
 	/**
-	 * Header (of document)
+	 * Microsoft Open XML header for spreadsheets
+	 * @var string
+	 * @static
+	 *
+	 */
+	const HEADER = "<?xml version=\"1.0\" encoding=\"%s\"?\>\n<Workbook xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\" xmlns:x=\"urn:schemas-microsoft-com:office:excel\" xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\" xmlns:html=\"http://www.w3.org/TR/REC-html40\">";
+	
+	/**
+	 * Microsoft Open Xml closing tag
+	 * @var string
+	 * @static
+	 *
+	 */
+	const FOOTER = '</Workbook>';
+	
+	/**
+	 * Worksheet data
+	 * @var array
+	 */
+	protected $aWorksheetData;
+	
+	/**
+	 * Output string
 	 * @var string
 	 */
-        private $header = "<?xml version=\"1.0\" encoding=\"%s\"?\>\n<Workbook xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\" xmlns:x=\"urn:schemas-microsoft-com:office:excel\" xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\" xmlns:html=\"http://www.w3.org/TR/REC-html40\">";
+	protected $sOutput;
+	
+	/**
+	 * Encoding
+	 * @var string
+	 */
+	protected $sEncoding;
 
-        /**
-         * Footer (of document)
-         * @var string
-         */
-        private $footer = "</Workbook>";
+	/**
+	 * Get sanitized workbook title
+	 *
+	 * Sanitizes a workbook title (filename) to contain only the
+	 * allowed characters using a whitelist regexp.
+	 *
+	 * @param string $title Title to sanitize
+	 *       
+	 * @return string Sanitized workbook title
+	 */
+	private function _getSanitizedWorkbookTitle($title)
+	{
+		return preg_replace('/[^aA-zZ0-9\_\-\.]/', '', $title);
+	}
 
-        /**
-         * Lines to output in the excel document
-         * @var array
-         */
-        private $lines = array();
+	/**
+	 * Get sanitized worksheet title
+	 *
+	 * Sanitizes a worksheet title using a whitelist regexp.
+	 * Excel only allows certain characters and only a certain
+	 * length in worksheet titles.
+	 *
+	 * @param string $title Title to sanitize
+	 *       
+	 * @return string Sanitized worksheet title
+	 */
+	private function _getSanitizedWorksheetTitle($title)
+	{
+		$sWorksheetTitle = preg_replace('/[\\\|:|\/|\?|\*|\[|\]]/', '', $title);
+		return substr($sWorksheetTitle, 0, 31);
+	}
 
-        /**
-         * Used encoding
-         * @var string
-         */
-        private $sEncoding;
-        
-        /**
-         * Convert variable types
-         * @var boolean
-         */
-        private $bConvertTypes;
-        
-        /**
-         * Worksheet title
-         * @var string
-         */
-        private $sWorksheetTitle;
+	/**
+	 * Generate workbook
+	 *
+	 * Generates the outer workbook frame to contain multiple worksheets.
+	 * Expects to have all required sheets as data already in the
+	 * $aWorksheetData container.
+	 *
+	 * @uses $aWorksheetData
+	 * @uses generateWorksheet()
+	 *      
+	 * @return void
+	 */
+	protected function generateWorkbook()
+	{
+		$this->sOutput = stripslashes(sprintf(self::HEADER, $this->sEncoding)) . "\n";
+		foreach($this->aWorksheetData as $aSheet){
+			$this->generateWorksheet($aSheet);
+		}
+		
+		$this->sOutput .= self::FOOTER;
+	}
 
-        /**
-         * Constructor
-         * 
-         * The constructor allows the setting of some additional
-         * parameters so that the library may be configured to
-         * one's needs.
-         * 
-         * On converting types:
-         * When set to true, the library tries to identify the type of
-         * the variable value and set the field specification for Excel
-         * accordingly. Be careful with article numbers or postcodes
-         * starting with a '0' (zero)!
-         * 
-         * @param string $sEncoding Encoding to be used (defaults to UTF-8)
-         * @param boolean $bConvertTypes Convert variables to field specification
-         * @param string $sWorksheetTitle Title for the worksheet
-         */
-        public function __construct($sEncoding = 'UTF-8', $bConvertTypes = false, $sWorksheetTitle = 'Table1')
-        {
-                $this->bConvertTypes = $bConvertTypes;
-        	$this->setEncoding($sEncoding);
-        	$this->setWorksheetTitle($sWorksheetTitle);
-        }
-        
-        /**
-         * Set encoding
-         * @param string Encoding type to set
-         */
-        public function setEncoding($sEncoding)
-        {
-        	$this->sEncoding = $sEncoding;
-        }
+	/**
+	 * Generate worksheet
+	 *
+	 * Generates a single worksheet into the output container.
+	 *
+	 * @param array $aSheet Array with sheet data
+	 *       
+	 * @uses generateRow()
+	 * @todo Check whether the row restriction is still necessary
+	 * @return void
+	 */
+	protected function generateWorksheet($aSheet)
+	{
+		$this->sOutput .= sprintf("<Worksheet ss:Name=\"%s\">\n    <Table>\n", $aSheet['title']);
+		if(count($aSheet['data'])){
+			$aSheet['data'] = array_slice($aSheet['data'], 0, 65536);
+			foreach($aSheet['data'] as $k=>$v){
+				$this->generateRow($v);
+			}
+		}
+		
+		$this->sOutput .= "    </Table>\n</Worksheet>\n";
+	}
 
-        /**
-         * Set worksheet title
-         * 
-         * Strips out not allowed characters and trims the
-         * title to a maximum length of 31.
-         * 
-         * @param string $title Title for worksheet
-         */
-        public function setWorksheetTitle ($title)
-        {
-                $title = preg_replace ("/[\\\|:|\/|\?|\*|\[|\]]/", "", $title);
-                $title = substr ($title, 0, 31);
-                $this->sWorksheetTitle = $title;
-        }
+	/**
+	 * Generate row
+	 *
+	 * Generates a single row in the worksheet.
+	 *
+	 * @param array $row Row item with data
+	 *       
+	 * @uses generateCell()
+	 * @return void
+	 */
+	protected function generateRow($row)
+	{
+		$this->sOutput .= "        <Row>\n";
+		foreach($row as $k=>$v){
+			$this->generateCell($v);
+		}
+		
+		$this->sOutput .= "        </Row>\n";
+	}
 
-        /**
-         * Add row
-         * 
-         * Adds a single row to the document. If set to true, self::bConvertTypes
-         * checks the type of variable and returns the specific field settings
-         * for the cell.
-         * 
-         * @param array $array One-dimensional array with row content
-         */
-        private function addRow ($array)
-        {
-        	$cells = "";
-                foreach ($array as $k => $v):
-                        $type = 'String';
-                        if ($this->bConvertTypes === true && is_numeric($v)):
-                                $type = 'Number';
-                        endif;
-                        $v = htmlentities($v, ENT_COMPAT, $this->sEncoding);
-                        $cells .= "<Cell><Data ss:Type=\"$type\">" . $v . "</Data></Cell>\n"; 
-                endforeach;
-                $this->lines[] = "<Row>\n" . $cells . "</Row>\n";
-        }
+	/**
+	 * Generate single cell
+	 *
+	 * This is where all the magic happens. Generates the cell from the given
+	 * input.
+	 *
+	 * @param string $cell Cell data
+	 *       
+	 * @return void
+	 */
+	protected function generateCell($cell)
+	{
+		// set default type
+		$type = 'String';
+		$cell = str_replace('&#039;', '&apos;', htmlspecialchars($cell, ENT_QUOTES));
+		$this->sOutput .= sprintf("            <Cell><Data ss:Type=\"%s\">%s</Data></Cell>\n", $type, $cell);
+	}
 
-        /**
-         * Add an array to the document
-         * @param array 2-dimensional array
-         */
-        public function addArray ($array)
-        {
-                foreach ($array as $k => $v)
-                        $this->addRow ($v);
-        }
+	/**
+	 * Constructor
+	 *
+	 * Sets default property values and allows to configure
+	 * the used encoding. If a certain encoding is used, Excel_Xml
+	 * expects all other input in this encoding.
+	 *
+	 * @param string $sEncoding Encoding to use (default UTF-8) [optional]
+	 *       
+	 * @return void
+	 */
+	public function __construct($sEncoding = 'UTF-8')
+	{
+		$this->sEncoding = $sEncoding;
+		$this->sOutput = '';
+	}
 
+	/**
+	 * Add a worksheet
+	 *
+	 * Adds a new worksheet to the data container.
+	 *
+	 * @param string $title Title of worksheet
+	 * @param array $data Two-dimensional array of data
+	 *       
+	 * @return void
+	 */
+	public function addWorksheet($title, $data)
+	{
+		$this->aWorksheetData[] = array('title'=>$this->_getSanitizedWorksheetTitle($title),'data'=>$data);
+	}
 
-        /**
-         * Generate the excel file
-         * @param string $filename Name of excel file to generate (...xls)
-         */
-        public function generateXML ($filename = 'excel-export')
-        {
-                // correct/validate filename
-                $filename = preg_replace('/[^aA-zZ0-9\_\-]/', '', $filename);
-    	
-                // deliver header (as recommended in php manual)
-                header("Content-Type: application/vnd.ms-excel; charset=" . $this->sEncoding);
-                header("Content-Disposition: inline; filename=\"" . $filename . ".xls\"");
+	/**
+	 * Send workbook to browser
+	 *
+	 * Sends the workbook to the browser opening the default "Save as..."
+	 * dialog by using PHP's header directive.
+	 *
+	 * @param string $filename Filename to use in dialog
+	 *       
+	 * @return void
+	 */
+	public function sendWorkbook($filename)
+	{
+		if(!preg_match('/\.(xml|xls)$/', $filename)){
+			throw new InvalidArgumentException('Filename mimetype must be .xml or .xls');
+		}
+		$filename = $this->_getSanitizedWorkbookTitle($filename);
+		$this->generateWorkbook();
+		if(preg_match('/\.xls$/', $filename)){
+			header("Content-Type: application/vnd.ms-excel; charset=" . $this->sEncoding);
+			if(preg_match("/MSIE/", $_SERVER['HTTP_USER_AGENT'])){
+				header("Content-Disposition: inline; filename=\"" . urlencode($filename) . "\"");
+			}else{
+				header("Content-Disposition: inline; filename*=\"" . $filename . "\"");
+			}
+		}else{
+			header("Content-Type: application/xml; charset=" . $this->sEncoding);
+			header("Content-Disposition: attachment; filename=\"" . $filename . "\"");
+		}
+		echo $this->sOutput;
+	}
 
-                // print out document to the browser
-                // need to use stripslashes for the damn ">"
-                echo stripslashes (sprintf($this->header, $this->sEncoding));
-                echo "\n<Worksheet ss:Name=\"" . $this->sWorksheetTitle . "\">\n<Table>\n";
-                foreach ($this->lines as $line)
-                        echo $line;
+	/**
+	 *
+	 * @param string $filename Filename to use in download dialog save
+	 */
+	public function download($filename)
+	{
+		$filepath = sys_get_temp_dir . '/cache/excel_' . time();
+		$this->generateWorkbook();
+		$this->_getSanitizedWorkbookTitle($filename);
+		if(!$handle = @fopen($filepath, 'w+')){
+			throw new Exception(sprintf("Not allowed to write to file %s", $filepath));
+		}
+		if(@fwrite($handle, $this->sOutput) === false){
+			throw new Exception(sprintf("Error writing to file %s", $filepath));
+		}
+		@fclose($handle);
+		
+		header("Content-type: application/octet-stream");
+		// 处理中文文件名
+		$ua = $_SERVER["HTTP_USER_AGENT"];
+		$encoded_filename = urlencode($filename);
+		$encoded_filename = str_replace("+", "%20", $encoded_filename);
+		if(preg_match("/MSIE/", $ua)){
+			header('Content-Disposition: attachment; filename="' . $encoded_filename . '"');
+		}else if(preg_match("/Firefox/", $ua)){
+			header("Content-Disposition: attachment; filename*=\"utf8''" . $filename . '"');
+		}else{
+			header('Content-Disposition: attachment; filename="' . $filename . '"');
+		}
+		header("Content-Length: " . filesize($filepath));
+		readfile($filepath);
+		@unlink($filepath);
+	}
 
-                echo "</Table>\n</Worksheet>\n";
-                echo $this->footer;
-        }
+	/**
+	 * Write workbook to file
+	 *
+	 * Writes the workbook into the file/path given as a parameters.
+	 * The method checks whether the directory is writable and the
+	 * file is not existing and writes the file.
+	 *
+	 * @param string $filename Filename to use for writing (must contain mimetype)
+	 *       
+	 * @return string Message
+	 */
+	public function writeWorkbook($filename)
+	{
+		$this->generateWorkbook();
+		$filename = $this->_getSanitizedWorkbookTitle($filename);
+		if(!$handle = @fopen($filename, 'w+')){
+			throw new Exception(sprintf("Not allowed to write to file %s", $filename));
+		}
+		if(@fwrite($handle, $this->sOutput) === false){
+			throw new Exception(sprintf("Error writing to file %s", $filename));
+		}
+		@fclose($handle);
+		return sprintf("File %s written", $filename);
+	}
 
+	/**
+	 * Get workbook
+	 *
+	 * Returns the generated workbook.
+	 *
+	 * @return string Generated workbook XML
+	 */
+	public function getWorkbook()
+	{
+		$this->generateWorkbook();
+		return $this->sOutput;
+	}
+
+	/**
+	 * Destructor
+	 *
+	 * The worksheet may contain some massive amount of data. The deconstructor
+	 * frees some of the memory again.
+	 *
+	 * @return void
+	 */
+	public function __destruct()
+	{
+		unset($this->aWorksheetData);
+		unset($this->sOutput);
+	}
 }
-
-?>
