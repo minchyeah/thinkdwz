11623018a3364e9f137f0d9d937e544a09d16453
Minch <yeah@minch.me>
2016-07-13 23:37:42 +0800

    update article

--- app/Modules/Home/Action/EmptyAction.class.php
+++ app/Modules/Home/Action/EmptyAction.class.php
@@ -9,7 +9,7 @@ class EmptyAction extends HomeAction
 	
     public function _empty($name)
     {
-        if (in_array(strtolower(MODULE_NAME), array('major','webucation','news','material','guide','recruit','liniankaoshi','zsnews','chengrengaokao'))){
+        if (in_array(strtolower(MODULE_NAME), array('news','activity'))){
     		$_GET['catalog'] = strtolower(MODULE_NAME);
     		if(is_numeric($name)){
     		    $_GET['id'] = intval($name);
@@ -23,7 +23,7 @@ class EmptyAction extends HomeAction
     			$_GET['page'] = intval($args[3]);
     		}
     		return A('Article')->category();
-    	}elseif (in_array(strtolower(MODULE_NAME), array('about','contact'))){
+    	}elseif (in_array(strtolower(MODULE_NAME), array('about','contact','honour','culture','welcome'))){
     		$_GET['code'] = strtolower(MODULE_NAME);
     		A('Article')->page();
     	}else {
