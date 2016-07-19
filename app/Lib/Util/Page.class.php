fdcaf0b276237e9b3b99e9cc7f224e86d940f949
梁敏成 <yeah@minch.me>
2016-07-19 18:10:56 +0800

    更新分页类库

--- app/Lib/Util/Page.class.php
+++ app/Lib/Util/Page.class.php
@@ -36,6 +36,18 @@ class Page {
 	public $prePage = '';
 	//下一页地址
 	public $nextPage = '';
+	
+	public $firstPageHtml = '<a href="__URL__" >__TITLE__</a>';
+	
+	public $prevPageHtml = '<a href="__URL__" >__TITLE__</a>';
+	
+	public $nextPageHtml = '<a href="__URL__" >__TITLE__</a>';
+	
+	public $lastPageHtml = '<a href="__URL__" >__TITLE__</a>';
+	
+	public $linkPageHtml = '<a href="__URL__" >__TITLE__</a>';
+	
+	public $currentPageHtml = '&nbsp;<span class="current">__TITLE__</span>';
 
 	/**
 	 * 架构函数
@@ -158,5 +170,96 @@ class Page {
 			array($this->config['header'],$this->nowPage,$this->totalRows,$this->totalPages,$upPage,$downPage,$theFirst,$prePage,$linkPage,$nextPage,$theEnd),$this->config['theme']);
 		return $pageStr;
 	}
-
+	
+	/**
+	 * 分页显示输出
+	 * @access public
+	 */
+	public function render()
+	{
+		if(0 == $this->totalRows) return '';
+		$p			  =   $this->varPage;
+	
+		// 分析分页参数
+		if($this->url){
+			if(false === strpos($this->url, '__PAGE__')){
+				$depr	   =   C('URL_PATHINFO_DEPR');
+				$url		=   rtrim(U('/'.$this->url,'',false),$depr).$depr.'__PAGE__';
+			}else{
+				$url 		=	$this->url;
+			}
+		}else{
+			if($this->parameter && is_string($this->parameter)) {
+				parse_str($this->parameter,$parameter);
+			}elseif(is_array($this->parameter)){
+				$parameter	  =   $this->parameter;
+			}elseif(empty($this->parameter)){
+				unset($_GET[C('VAR_URL_PARAMS')]);
+				$var =  !empty($_POST)?$_POST:$_GET;
+				if(empty($var)) {
+					$parameter  =   array();
+				}else{
+					$parameter  =   $var;
+				}
+			}
+			$parameter[$p]  =   '__PAGE__';
+			$url			=   U('',$parameter);
+		}
+		//上下翻页字符串
+		$upRow		  =   $this->nowPage-1;
+		$downRow		=   $this->nowPage+1;
+		if ($upRow>0){
+			$this->prePage = str_replace('__PAGE__',$upRow,$url);
+			$upPage	 = str_replace(array('__URL__', '__TITLE__'), array(str_replace('__PAGE__',$upRow,$url), $this->config['prev']), $this->prevPageHtml);
+		}else{
+			$upPage	 =   '';
+		}
+	
+		if ($downRow <= $this->totalPages){
+			$this->nextPage = str_replace('__PAGE__',$downRow,$url);
+			$downPage	 = str_replace(array('__URL__', '__TITLE__'), array(str_replace('__PAGE__',$downRow,$url), $this->config['next']), $this->nextPageHtml);
+		}else{
+			$downPage   =   '';
+		}
+		// << < > >>
+		if($this->nowPage-$this->rollPage > 1){
+			$theFirst   =   str_replace(array('__URL__', '__TITLE__'), array(str_replace('__PAGE__',1,$url), $this->config['first']), $this->firstPageHtml);
+			$prePage	=	'...';
+		}else{
+			$theFirst   =   '';
+			$prePage	=	'';
+		}
+		if($this->nowPage+$this->rollPage < $this->totalPages){
+			$theEnd   =   str_replace(array('__URL__', '__TITLE__'), array(str_replace('__PAGE__',$this->totalPages,$url), $this->config['last']), $this->lastPageHtml);
+			$nextPage	=	'...';
+		}else{
+			$theEnd	 =   '';
+			$nextPage	=	'';
+		}
+		// 1 2 3 4 5
+		$linkPage = "";
+		$i = $this->nowPage - $this->rollPage;
+		$i = $i<1 ? 1 : $i;
+	
+		$j = $this->nowPage + $this->rollPage;
+		$j = $j>$this->totalPages ? $this->totalPages : $j;
+		for($i; $i<=$j; $i++){
+			$page = $i;
+			if($page!=$this->nowPage){
+				if($page<=$this->totalPages){
+					$linkPage .= str_replace(array('__URL__', '__TITLE__'), array(str_replace('__PAGE__',$page,$url), $page), $this->linkPageHtml);
+				}else{
+					break;
+				}
+			}else{
+				if($this->totalPages != 1){
+					$linkPage .= str_replace('__TITLE__', $page, $this->currentPageHtml);
+				}
+			}
+		}
+		$pageStr	 =   str_replace(
+				array('%header%','%nowPage%','%totalRow%','%totalPage%','%upPage%','%downPage%','%first%','%prePage%','%linkPage%','%nextPage%','%end%'),
+				array($this->config['header'],$this->nowPage,$this->totalRows,$this->totalPages,$upPage,$downPage,$theFirst,$prePage,$linkPage,$nextPage,$theEnd),$this->config['theme']);
+		return $pageStr;
+	}
 }
